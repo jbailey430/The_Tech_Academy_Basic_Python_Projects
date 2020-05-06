576c2d0061ad5879649203313d2fc68515bcb4dd
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-05-06 11:20:30 -0700

    update

--- drill.py
+++ drill.py
@@ -12,31 +12,18 @@ print("Files and directories in '",'C:\The-Tech-Academy-Basic-Python-Projects\',
 print(dir_list)
 
 
-fName = 'test.txt, test2.txt'
-
 fPath = 'C:\\The-Tech-Academy-Basic-Python-Projects\\'
 
-data = ["test.txt", "test2.txt"]
-for x in data:
-    print(x)
-    if x == "test2.txt":
-        break
+for fPath in os.listdir(path='C:\\The-Tech-Academy-Basic-Python-Projects\\'):
+
+    if (fPath.endswith('.txt')):
+        print(fPath)
+        print(os.path.getmtime('C:\\The-Tech-Academy-Basic-Python-Projects\\'))
+        continue
+    else:
+        continue
 
 
-abPath = os.path.join(fPath, fName)
-print(abPath)
 
 
-
-path = 'C:\\The-Tech-Academy-Basic-Python-Projects\\'
-try:
-    modification_time = os.path.getmtime(path)
-    print("Last modification time since the epoch:", modification_time)
-
-except OSError:
-    print("Path '%s' does not exsists or is inaccessible" %path)
-    sys.exit()
-
-local_time = time.ctime(modification_time)
-print("Last modification time (Local time):", local_time)
 
